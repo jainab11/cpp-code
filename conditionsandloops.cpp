30d17feb82fe3dc53170f21698b1f0bb3089aaea
Jainab <92934261+jainab11@users.noreply.github.com>
2023-05-20 21:45:50 +0530

    Add files via upload
    basic if condition

---
+++ conditionsandloops.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    
+    cout<<"Enter the value of A"<<endl;
+    cin>>a;
+    cout<<"Enter the value of B"<<endl;
+    cin>>b;
+    if(a>b){
+        cout<<"A is grater than B"<<endl;
+        
+    }else{
+        cout<<"B is greter than A"<<endl;
+    }
+
+    // if(n>0){
+    //     cout<<"n is a positive number"<<endl;
+
+    // }
+    // else{
+    //     cout<<"n is a neagtive number"<<endl;
+
+    // }
+}
